70df07f442b21c145dacf1b2c5dddab94e332795
MahdiyarGHD <m.gh.1385.mahdiyar@gmail.com>
2024-03-26 04:05:44 +0330

    Added more project files

---
+++ src/CSharp/Backend/ParehNegar.Database/Database/Contexts/ParehNegarContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParehNegar.Database.Database.Contexts
+{
+    public sealed class AppTaxContext : DbContext
+    {
+        public AppTaxContext(DbContextOptions builder) : base(builder)
+        {
+        }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
