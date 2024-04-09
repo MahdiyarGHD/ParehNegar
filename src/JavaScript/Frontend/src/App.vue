dfd80551cddc56eea3f413cee2f19825e8473c8c
MahdiyarGHD <m.gh.1385.mahdiyar@gmail.com>
2024-04-09 20:14:55 +0330

    fix: moved static contents base to it's component

--- src/JavaScript/Frontend/src/App.vue
+++ src/JavaScript/Frontend/src/App.vue
@@ -3,79 +3,7 @@ import { RouterLink, RouterView, useRouter } from 'vue-router';
 import Header from './components/Header.vue'
 import Tabs from "./components/Tabs.vue";
 import { useTabsStore } from './stores/useTabsStore.js'
-import contentApi from './client/contentApi.js'
-window.addEventListener("load", function() { 
-  const container = document.getElementById("app");
-  const elements = container.querySelectorAll("[static-content]");
-  
-  elements.forEach(element => {
-    const button = document.createElement("button");
-    button.textContent = "Edit"; 
-    button.style.position = "absolute";
-    button.classList.add("content-edit-btn"); 
-    button.style.top = element.offsetTop + "px"; 
-    button.style.left = element.offsetLeft + "px"; 
-    button.id = `edit-content-btn__${element.getAttribute("static-content")}`;
-    
-    button.addEventListener('click', (ele) => {
-      const textarea = document.createElement("textarea");
-      contentApi.apiContentGetByLanguagePost({
-        body: { 
-          key: element.getAttribute("static-content"),
-          language: ""
-        }
-      }, (err, response) => textarea.innerHTML = response?.result?.data ?? "")
-      
-      textarea.style.position = "absolute";
-      textarea.style.top = element.offsetTop + "px"; 
-      textarea.style.left = element.offsetLeft + "px"; 
-      textarea.id = `edit-content-input__${element.getAttribute("static-content")}`;
-      container.appendChild(textarea);
-      
-      const saveButton = document.createElement("button");
-      saveButton.textContent = "Save"; 
-      saveButton.style.position = "absolute";
-      saveButton.classList.add("content-edit-btn"); 
-      saveButton.style.top = textarea.offsetTop + 20 + "px"; 
-      saveButton.style.left = textarea.offsetLeft + "px"; 
-      saveButton.id = `save-content-btn__${element.getAttribute("static-content")}`;
-      
-      saveButton.addEventListener('click', (el) => {
-        contentApi.apiContentAddContentWithKeyPost({
-          body: {
-            key: element.getAttribute("static-content"),
-            languageData: [
-              {
-                language: "fa-IR",
-                data: textarea.value
-              }
-            ]
-          }
-        }, (err, response) => {
-          saveButton.remove();
-          textarea.remove();
-        })
-      })
-      
-      container.appendChild(saveButton);
-      
-    });
-
-    container.appendChild(button);
-    
-    
-      contentApi.apiContentGetByLanguagePost({
-            body: { 
-              key: element.getAttribute("static-content"),
-              language: ""
-            }
-      }, (err, response) => element.innerHTML = response?.result?.data)
-  });
-  
-});
-
-
-
+import StaticContents from './components/StaticContents.vue';
 const store = useTabsStore();
 </script>
 
@@ -94,7 +22,7 @@ const store = useTabsStore();
       <RouterView />
     </div>
     
-
+    <StaticContents/>
 </template>
 
 <style scoped>
---
+++ src/JavaScript/Frontend/src/components/StaticContents.vue
@@ -0,0 +1,74 @@
+<script setup>
+import contentApi from '../client/contentApi.js'
+
+window.addEventListener("load", function() { 
+  const container = document.getElementById("app");
+  const elements = container.querySelectorAll("[static-content]");
+  
+  elements.forEach(element => {
+    const button = document.createElement("button");
+    button.textContent = "Edit"; 
+    button.style.position = "absolute";
+    button.classList.add("content-edit-btn"); 
+    button.style.top = element.offsetTop + "px"; 
+    button.style.left = element.offsetLeft + "px"; 
+    button.id = `edit-content-btn__${element.getAttribute("static-content")}`;
+    
+    button.addEventListener('click', (ele) => {
+      const textarea = document.createElement("textarea");
+      contentApi.apiContentGetByLanguagePost({
+        body: { 
+          key: element.getAttribute("static-content"),
+          language: ""
+        }
+      }, (err, response) => textarea.innerHTML = response?.result?.data ?? "")
+      
+      textarea.style.position = "absolute";
+      textarea.style.top = element.offsetTop + "px"; 
+      textarea.style.left = element.offsetLeft + "px"; 
+      textarea.id = `edit-content-input__${element.getAttribute("static-content")}`;
+      container.appendChild(textarea);
+      
+      const saveButton = document.createElement("button");
+      saveButton.textContent = "Save"; 
+      saveButton.style.position = "absolute";
+      saveButton.classList.add("content-edit-btn"); 
+      saveButton.style.top = textarea.offsetTop + 20 + "px"; 
+      saveButton.style.left = textarea.offsetLeft + "px"; 
+      saveButton.id = `save-content-btn__${element.getAttribute("static-content")}`;
+      
+      saveButton.addEventListener('click', (el) => {
+        contentApi.apiContentAddContentWithKeyPost({
+          body: {
+            key: element.getAttribute("static-content"),
+            languageData: [
+              {
+                language: "fa-IR",
+                data: textarea.value
+              }
+            ]
+          }
+        }, (err, response) => {
+          saveButton.remove();
+          textarea.remove();
+        })
+      })
+      
+      container.appendChild(saveButton);
+      
+    });
+
+    container.appendChild(button);
+    
+    
+      contentApi.apiContentGetByLanguagePost({
+            body: { 
+              key: element.getAttribute("static-content"),
+              language: ""
+            }
+      }, (err, response) => element.innerHTML = response?.result?.data)
+  });
+  
+});
+
+</script>
